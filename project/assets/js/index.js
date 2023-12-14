6960ff4c2fe1134e79b4daae8ca7e9c6ed253618
Wendyle Christian M. Seno <106366417+Wendyle18@users.noreply.github.com>
2023-12-15 00:24:03 +0800

    Add files via upload

--- project/assets/js/index.js
+++ project/assets/js/index.js
@@ -1,43 +1,63 @@
 $(document).ready(function () {
-                
-    var currentSection = "#personalDetails"; //Declare default 
-    var isAnimating = false; //tracker of animation
+    var currentSection = "#personalDetails";
+    var isAnimating = false;
 
     function showSection(section) {
         if (!isAnimating && currentSection !== section) {
             isAnimating = true;
             $(currentSection).fadeOut(500, function () {
                 $(section).fadeIn(500, function () {
-                    $(this).css('display', 'block'); 
+                    $(this).css('display', 'block');
                     $(this).css('opacity', '1');
-
-                    isAnimating = false; 
+                    isAnimating = false;
                     currentSection = section;
                 });
             });
         }
     }
 
-            $("#familyBackground").hide();
-            $("#educationBackground").hide();
+    $("#familyBackground, #educationBackground").hide();
+
+    // Click first link
+    $("#first-link").click(function () {
+        showSection("#personalDetails");
+    });
+
+    // Click second link
+    $("#second-link").click(function () {
+        showSection("#familyBackground");
+    });
+
+    // Click third link
+    $("#third-link").click(function () {
+        showSection("#educationBackground");
+    });
 
 
-            // Click first link
-            $("#first-link").click(function () {    
-                showSection("#personalDetails");
-                
-            });
+    
+    // Click mobile back button
+    $(".btn-back").click(function () {
+        var prevSection;
+        if (currentSection === "#familyBackground") {
+            prevSection = "#personalDetails";
+        } else if (currentSection === "#educationBackground") {
+            prevSection = "#familyBackground";
+        } else {
+            prevSection = "#personalDetails";
+        }
+        showSection(prevSection);
+    });
 
-            // Click second link
-            $("#second-link").click(function () {
-                showSection("#familyBackground");
-            });
-
-            // Click third link
-            $("#third-link").click(function () {
-                showSection("#educationBackground");
-            });
+    // Click mobile next button
+    $(".btn-next").click(function () {
+        var nextSection;
+        if (currentSection === "#personalDetails") {
+            nextSection = "#familyBackground";
+        } else if (currentSection === "#familyBackground") {
+            nextSection = "#educationBackground";
+        } else {
+            nextSection = "#personalDetails";
+        }
+        showSection(nextSection);
+    });
 });
-
-
-
